17b10592fcb2162cf2c2c4341de4d58544165467
ching <prasadgowda128@gmail.com>
2025-07-03 01:32:19 +0530

    Add initial implementations for immutable, mutable, and numbers data types

---
+++ 02_datatypes/immutable.py
@@ -0,0 +1,8 @@
+sugarQuantity = 2
+print("Initial sugar quantity is - ", sugarQuantity)
+
+sugarQuantity = 12
+print("Updated sugar quantity is - ", sugarQuantity)
+
+print("the id of 2 is - ", id(2))
+print("the id of 12 is - ", id(12))
---
+++ 02_datatypes/mutable.py
@@ -0,0 +1,7 @@
+groupA = set()
+print("Initial groupA ID is - ", id(groupA))
+print("Initial groupA is - ", groupA)
+
+groupA.add(1)
+print("Updated groupA ID is - ", id(groupA))
+print("updated groupA is - ", groupA)
